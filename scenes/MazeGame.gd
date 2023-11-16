98837486699adeed3d7eb5e97b8b829dd85e6d33
Josh Edwards <josh@terasoft.com.au>
2023-11-16 11:43:38 +1100

    Add player movement and tile setting

--- scenes/MazeGame.gd
+++ scenes/MazeGame.gd
@@ -7,8 +7,19 @@ var floor_cell = Vector2i(2,0)
 var maze: TileMap
 var player: CharacterBody2D
 
+var move_step = 2
+var cell_size = 16
+var cell_dist = 0
+
 var is_travelling = false
+var is_stopping = false
 var cur_direction = "right"
+var directions = {
+	"right": Vector2i(1,0),
+	"down": Vector2i(0,1),
+	"left": Vector2i(-1,0),
+	"up": Vector2i(0,-1),
+}
 
 func _ready():
 	maze = get_child(0)
@@ -17,13 +28,26 @@ func _ready():
 	print(maze.get_cell_atlas_coords(0, maze.local_to_map(player.position)))
 	
 func _physics_process(delta):
-	if !is_travelling:
-		return
+	if  int(player.position.x) % cell_size == 0 && int(player.position.y) % cell_size == 0:
+		var cur_pos = maze.local_to_map(player.position)
+		var cur_cell = maze.get_cell_atlas_coords(0, cur_pos)
+		if cur_cell != slime_cell:
+			maze.set_cell(0, cur_pos, 2, slime_cell)
+		
+		var dir = directions[cur_direction]
+		var next_pos = cur_pos + dir
+		var next_cell = maze.get_cell_atlas_coords(0, next_pos)
+		if next_cell == wall_cell:
+			is_travelling = false
+			return
+	var dir = directions[cur_direction]
+	player.position += Vector2(dir.x, dir.y)*move_step
+
+		
 	
-	var cur_pos = maze.local_to_map(player.position)
-	var cur_cell = maze.get_cell_atlas_coords(0, cur_pos)
-	if cur_cell != slime_cell:
-		maze.set_cell(0, cur_pos, 2, slime_cell)
+	
+	
+	
 	
 func _unhandled_key_input(event):
 	if is_travelling:
